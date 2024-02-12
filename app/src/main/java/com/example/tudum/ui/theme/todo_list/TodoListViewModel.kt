39e455a52bd538e508b746513155a2a9da2201d2
Sameer <sameer.ali097643@gmail.com>
2024-02-12 15:54:33 +0530

    ViewModel, Events Created

--- app/src/main/java/com/example/tudum/ui/theme/todo_list/TodoListViewModel.kt
+++ app/src/main/java/com/example/tudum/ui/theme/todo_list/TodoListViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.tudum.ui.theme.todo_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tudum.Util.Routes
 import com.example.tudum.Util.UiEvent
 import com.example.tudum.data.Repo
+import com.example.tudum.data.Task
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,5 +21,55 @@ class TodoListViewModel @Inject constructor(
 
     private val _uiEvent = Channel<UiEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
+
+    private var deleteTask: Task? = null
+
+    fun onEvent(event: TodoListEvent){
+        when(event){
+
+            is TodoListEvent.onTodoClick -> {
+                sendUiEvent(UiEvent.Navigate(Routes.ADD_EDIT_TODO + "?todoId=${event.task.id}"))
+            }
+
+            is TodoListEvent.OnAddTaskClick -> {
+                sendUiEvent(UiEvent.Navigate(Routes.ADD_EDIT_TODO))
+            }
+
+            is TodoListEvent.onUndoDeleteClick -> {
+                deleteTask?.let { task ->
+                    viewModelScope.launch {
+                        repository.upsertTask(task)
+                    }
+                }
+            }
+
+            is TodoListEvent.DeleteTask -> {
+                viewModelScope.launch {
+                    repository.deleteTask(event.task)
+                    sendUiEvent(UiEvent.ShowSnackbar(
+                        message = "Task Deleted",
+                        action = "undo"
+                    ))
+                }
+            }
+
+            is TodoListEvent.OnDoneChange -> {
+                viewModelScope.launch {
+                    repository.upsertTask(
+                        event.task.copy(
+                            isDone = event.isDone
+                        )
+                    )
+                }
+            }
+
+        }
+    }
+
+    private fun sendUiEvent(event: UiEvent){
+        viewModelScope.launch {
+            _uiEvent.send(event)
+        }
+    }
 
 }
